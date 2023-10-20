b7831a4da15097c19f46115e906ff2cbccb0b498
Noah <ubipsy@gmail.com>
2023-10-20 19:57:02 +1000

    Updated Database Script

--- Assets/Scripts Folder/help.cs
+++ Assets/Scripts Folder/help.cs
@@ -57,12 +57,14 @@ public class help : MonoBehaviour
         }
 
         // Open a connection to the SQLite database
-        // changed persistentDatabasePath to database path
-        connection = new SQLiteConnection(databasePath, SQLiteOpenFlags.ReadWrite | SQLiteOpenFlags.Create);
-        connection.CreateTable<Gate_Fence_Database>();
+        SQLiteConnection connection = null;
 
         try
         {
+            // changed databasePath to persistentDatabasePath
+            connection = new SQLiteConnection(persistentDatabasePath, SQLiteOpenFlags.ReadWrite | SQLiteOpenFlags.Create);
+            connection.CreateTable<Gate_Fence_Database>();
+
             // Retrieve data from the database and populate the productsList
             List<Gate_Fence_Database> dbProducts = connection.Table<Gate_Fence_Database>().ToList();
             productsList = dbProducts; // Assign dbProducts to productsList
@@ -100,5 +102,89 @@ public class help : MonoBehaviour
         {
             Debug.LogError("Error: " + e.Message);
         }
+        finally
+        {
+            // Close the connection in a finally block to ensure it's closed even if an exception occurs
+            if (connection != null)
+            {
+                connection.Close();
+            }
+        }
     }
 }
+
+
+
+//private IEnumerator Start()
+//{
+//    // Define the path to your database file in the StreamingAssets folder
+//    string databasePath = System.IO.Path.Combine(Application.streamingAssetsPath, "Ezee_Industries.db");
+
+//    // Set the databasePath to the path in the persistent data path
+//    string persistentDatabasePath = System.IO.Path.Combine(Application.persistentDataPath, "Ezee_Industries.db");
+
+//    // Check if the file exists in the persistent data path (cache)
+//    if (!System.IO.File.Exists(persistentDatabasePath))
+//    {
+//        // Create a UnityWebRequest to download the file
+//        UnityWebRequest webRequest = UnityWebRequest.Get(databasePath);
+
+//        // Send the web request and wait for it to complete
+//        yield return webRequest.SendWebRequest();
+
+//        // Check if there was an error while downloading
+//        if (webRequest.result != UnityWebRequest.Result.Success)
+//        {
+//            Debug.LogError("Failed to download database: " + webRequest.error);
+//            yield break; // Exit the coroutine if there was an error
+//        }
+
+//        // Save the downloaded bytes to the persistent data path
+//        System.IO.File.WriteAllBytes(persistentDatabasePath, webRequest.downloadHandler.data);
+//    }
+
+//    // Open a connection to the SQLite database
+//    // changed persistentDatabasePath to database path
+//    connection = new SQLiteConnection(databasePath, SQLiteOpenFlags.ReadWrite | SQLiteOpenFlags.Create);
+//    connection.CreateTable<Gate_Fence_Database>();
+
+//    try
+//    {
+//        // Retrieve data from the database and populate the productsList
+//        List<Gate_Fence_Database> dbProducts = connection.Table<Gate_Fence_Database>().ToList();
+//        productsList = dbProducts; // Assign dbProducts to productsList
+
+//        Debug.Log("Number of products: " + productsList.Count);
+
+//        // Loop through productsList to create UI elements
+//        foreach (Gate_Fence_Database product in productsList)
+//        {
+//            if (product.category.Contains("Gate"))
+//            {
+//                Debug.Log("Testing for each starting");
+//                // Instantiate the prefab as a child of the DatabaseEntriesContainer
+//                GameObject productInstance = Instantiate(DBDisplayEntries, DatabaseEntriesContainer.transform);
+
+//                // Get references to UI elements
+//                TextMeshProUGUI nameText = productInstance.transform.Find("DB_Name").GetComponent<TextMeshProUGUI>();
+//                TextMeshProUGUI priceText = productInstance.transform.Find("DB_Price").GetComponent<TextMeshProUGUI>();
+//                TextMeshProUGUI categoryText = productInstance.transform.Find("DB_Category").GetComponent<TextMeshProUGUI>();
+//                Image productImage = productInstance.transform.Find("Image").GetComponent<Image>();
+
+//                // Set text and image using product data
+//                nameText.text = "Name: " + product.name;
+//                priceText.text = "Price: " + product.price;
+//                categoryText.text = "Category: " + product.category;
+
+//                // Create a Texture2D from the byte[] image data
+//                Texture2D tex = new Texture2D(2, 2);
+//                tex.LoadImage(product.image);
+//                productImage.sprite = Sprite.Create(tex, new Rect(0, 0, tex.width, tex.height), Vector2.zero);
+//            }
+//        }
+//    }
+//    catch (Exception e)
+//    {
+//        Debug.LogError("Error: " + e.Message);
+//    }
+//}
